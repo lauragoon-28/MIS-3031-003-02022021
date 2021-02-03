4de41e0a4c68be248eeb28a923085dd59e13dce0
Laura Goon <Laura.goon@ou.edu>
2021-02-02 18:18:30 -0600

    Created a new solution for an inclass examples

---
+++ VariablesInputConnectionFormating.example/VariablesInputConnectionFormating.example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VariablesInputConnectionFormating.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
