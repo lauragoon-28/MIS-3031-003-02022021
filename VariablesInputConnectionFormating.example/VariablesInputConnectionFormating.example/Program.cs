b2b5819c2ee72394174cf3262120a0eacb9ec2c8
Laura Goon <Laura.goon@ou.edu>
2021-02-02 18:33:23 -0600

    Completed example over variable inputs and something

--- VariablesInputConnectionFormating.example/VariablesInputConnectionFormating.example/Program.cs
+++ VariablesInputConnectionFormating.example/VariablesInputConnectionFormating.example/Program.cs
@@ -6,7 +6,30 @@ namespace VariablesInputConnectionFormating.example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; // idk why but we want to bump everyons gpa by .5
+
+            string output = "Hello" + firstName + " " + lastName + "! " + " we boosted your gpa to " + gpa.ToString("N3");
+
+            Console.WriteLine(output)
+
+                //output = $"Hello {firstName} {lastName}! we boosted your gpa to gpa.ToString("N3");
+
+
+
+
         }
     }
 }
